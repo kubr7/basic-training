8774f2e6e612cdef87c21d9db7e7931fee6acc57
kubr7 <kubrtech@gmail.com>
2025-06-24 14:47:06 +0000

    Added UserTaskCount - Helper Contract

---
+++ 05-to-do-dapp/backend/contracts/UserTaskCount.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract UserTaskCount{
+    address public todoContract;
+    mapping(address => uint256) public userTaskCounts;
+
+    constructor(){ 
+        // Deploy without TodoContract dependency
+    }
+
+    function setTodoContract(address _todoContract) external {
+        require(_todoContract != address(0), "Invalid TodoContract Address - Cant be zero");
+        require(todoContract == address(0), "TodoContract already set");
+        todoContract = _todoContract;
+    }
+
+    modifier onlyTodoContract(){
+        require(msg.sender == todoContract, "Only TodoContract can call this");
+        _;
+    }
+
+    function updateMapping(address user) external onlyTodoContract{
+        userTaskCounts[user]++;
+    }
+
+    function getUserTaskCount(address user) external view returns(uint256){
+        return userTaskCounts[user];
+    }
+}
