007237fc59c16b97d8566a489c609b369fa2a763
kubr7 <kubrtech@gmail.com>
2025-06-24 14:53:00 +0000

    Added Hardhat config

--- 05-to-do-dapp/backend/hardhat.config.js
+++ 05-to-do-dapp/backend/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  }
 };
