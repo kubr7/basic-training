7c7ab692671d0d7ff008d83da8d13be9b57e5765
kubr <kubrtech@gmail.com>
2025-06-27 17:06:28 +0000

    Updated config settings

--- 05-to-do-dapp/backend/hardhat.config.js
+++ 05-to-do-dapp/backend/hardhat.config.js
@@ -1,9 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   },
@@ -12,5 +25,14 @@ module.exports = {
       url: process.env.ALCHEMY_SEPOLIA_URL,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY]
     }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY, // Optional for live ETH price
+    showTimeSpent: true,
+    excludeContracts: [], // Optional: list contract names to skip
+    outputFile: "gas-report.txt", // Optional: write report to file
+    noColors: true // Useful for CI logs
   }
 };
