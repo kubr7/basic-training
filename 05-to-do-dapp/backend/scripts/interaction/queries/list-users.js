f3e827bafa827b757e0375132843fbe0a6e4b750
kubr <kubrtech@gmail.com>
2025-06-30 11:01:36 +0000

    script to display all users

--- 05-to-do-dapp/backend/scripts/interaction/queries/list-users.js
+++ 05-to-do-dapp/backend/scripts/interaction/queries/list-users.js
@@ -2,19 +2,23 @@ const { ethers } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-    console.log("Interacting with contracts...");
+    console.log("\nInteracting with contracts...");
     const contractAddress = process.env.TODO_CONTRACT_ADDRESS;
     if (!contractAddress) {
         console.log("To-Do Contract address is missing in .env file.");
         return;
     }
 
-    console.log("\nTo-Do Contract Address:", contractAddress);
-
     const toDoContract = await ethers.getContractAt("ToDoContract", contractAddress);
-
     const userTaskCount = await toDoContract.userTaskCountContract();
-    console.log("UserTaskCount Address:", userTaskCount);
+
+    console.log("\nContract Addresses:")
+    console.log("- To-Do Contract Address:", contractAddress);
+    console.log("- UserTaskCount Address:", userTaskCount);
+    
+    const activeTaskCount = await toDoContract.getActiveTaskCount();
+    console.log("\nStatus:");
+    console.log("- Active tasks:", activeTaskCount.toString());
 
     console.log("\nGetting all users...");
     const allUsers = await toDoContract.getAllUsers();
@@ -22,9 +26,9 @@ async function main() {
     
     console.log("\nFinal Summary:");
     const totalTasks = await toDoContract.taskCount();
-    console.log("\nTotal tasks ever created:", totalTasks.toString());
-    const activeTasks = await toDoContract.getActiveTaskCount();
-    console.log("Active tasks (non-deleted):", activeTasks.toString());
+    console.log("- Total Tasks [Ever created]:", totalTasks.toString());
+    console.log("- Active Tasks [Non-deleted]:", activeTaskCount.toString());
+
     const activeTasksList = await toDoContract.getActiveTasks();
 
     let modifiedCount = 0;
@@ -33,11 +37,7 @@ async function main() {
             modifiedCount++;
         }
     }
-
-    console.log("Modified active task count:", modifiedCount);
-
-    const updatedUsers = await toDoContract.getAllUsers();
-    console.log("\nAll users:", updatedUsers);
+    console.log("- Modified Tasks [Active]:", modifiedCount);
 }
 
 main()
