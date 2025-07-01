441d910804e7d2042ee9f595266240c74a7b3053
kubr <kubrtech@gmail.com>
2025-07-01 06:11:14 +0000

    Script to interact with assigned tasks

---
+++ 05-to-do-dapp/backend/scripts/interaction/queries/assigned-tasks.js
@@ -0,0 +1,64 @@
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+    console.log("\nInteracting with contracts...");
+
+    const contractAddress = process.env.TODO_CONTRACT_ADDRESS;
+    if (!contractAddress) {
+        console.log("To-Do Contract address is missing in .env file.");
+        return;
+    }
+
+    const toDoContract = await ethers.getContractAt("ToDoContract", contractAddress);
+    const userTaskCount = await toDoContract.userTaskCountContract();
+
+    console.log("\nContract Addresses:")
+    console.log("- To-Do Contract Address:", contractAddress);
+    console.log("- UserTaskCount Address:", userTaskCount);
+    
+    const activeTaskCount = await toDoContract.getActiveTaskCount();
+    console.log("\nStatus:");
+    console.log("- Active tasks:", activeTaskCount.toString());
+
+    const [signer] = await ethers.getSigners();
+    const userAddress = signer.address;
+
+    const assignedTasks = await toDoContract.getAllTaskByUserAsAssignee(userAddress);
+
+    console.log("\nTasks assigned to users:");
+    assignedTasks.forEach((task, index) => {
+        console.log(`  #${index + 1}:`, {
+            id: task.id.toString(),
+            assignedTo: task.assignedTo,
+            description: task.description,
+            date: task.date.toString(),
+            status: Number(task.status) === 0 ? "Pending" : "Completed"
+        });
+    });
+
+    console.log("\nFinal Summary:");
+    const totalTasks = await toDoContract.taskCount();
+    console.log("- Total Tasks [Ever created]:", totalTasks.toString());
+    console.log("- Active Tasks [Non-deleted]:", activeTaskCount.toString());
+
+    const activeTasksList = await toDoContract.getActiveTasks();
+
+    let modifiedCount = 0;
+    for (const task of activeTasksList) {
+        if (task.isModified) {
+            modifiedCount++;
+        }
+    }
+    console.log("- Modified Tasks [Active]:", modifiedCount);
+}
+
+main()
+    .then(() => {
+        console.log("\nDone! All contract interactions complete.");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error("Error occurred:", err.message);
+        process.exit(1);
+    });
