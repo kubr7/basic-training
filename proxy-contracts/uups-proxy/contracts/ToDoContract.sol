b377f577becdb788e4863e4b2afb6393d0a658a4
kubr7 <kubrtech@gmail.com>
2025-07-08 20:13:15 +0530

    Added upgradable contract

--- proxy-contracts/uups-proxy/contracts/ToDoContract.sol
+++ proxy-contracts/uups-proxy/contracts/ToDoContract.sol
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract ToDoContract {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract ToDoContract is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     enum TaskStatus {
         Pending,
         Completed
@@ -54,6 +58,16 @@ contract ToDoContract {
         TaskStatus status,
         uint256 timestamp
     );
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        taskCount = 0;
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 
     modifier onlyCreator(uint256 taskId) {
         require(
