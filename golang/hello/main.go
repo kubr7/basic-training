61d9a3410c157db9394b5f796c79160f7cf23427
kubr <kubrtech@gmail.com>
2025-08-01 04:07:38 +0530

    Updated name argument

--- golang/hello/main.go
+++ golang/hello/main.go
@@ -11,7 +11,8 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	result, err := greetings.Hello("Seven")
+	names := []string{"May", "June", "July"}
+	result, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
