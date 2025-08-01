0373ce78c7d8e4e5daeb9e90337e6407fdc54570
kubr <kubrtech@gmail.com>
2025-08-01 22:46:01 +0530

    method receivers (pointer vs value) behavior

--- golang/18_methods/main.go
+++ golang/18_methods/main.go
@@ -6,21 +6,46 @@ type rect struct {
 	width, height int
 }
 
-func (r *rect) area() int {
-	return r.width * r.height
+func (radius *rect) area() int {
+	return radius.width * radius.height
 }
 
-func (r rect) perim() int {
-	return 2*r.width + 2*r.height
+func (radius rect) perim() int {
+	return 2*radius.width + 2*radius.height
+}
+
+// Pointer receiver: actually modifies width & height
+func (radius *rect) resize(newW, newH int) {
+	radius.width = newW
+	radius.height = newH
 }
 
 func main() {
-	r := rect{width: 10, height: 5}
+	result := rect{width: 10, height: 6}
 
-	fmt.Println("area: ", r.area())
-	fmt.Println("perim:", r.perim())
+	// Pointer receiver on value (Go auto &result)
+	fmt.Println("area: ", result.area())
+	// Value receiver on value
+	fmt.Println("perim:", result.perim())
 
-	rp := &r
-	fmt.Println("area: ", rp.area())
-	fmt.Println("perim:", rp.perim())
+	fmt.Println("--------------------------------")
+
+	// Another rect value
+	res := rect{width: 10, height: 10}
+	fmt.Println("\narea: ", res.area())
+	fmt.Println("perim:", res.perim())
+
+	// Pointer to existing rect
+	resultPtr := &result
+	fmt.Println("\narea: ", resultPtr.area())
+	fmt.Println("perim:", resultPtr.perim())
+
+	// Pointer to new rect
+	resPtr := &rect{width: 15, height: 15}
+	fmt.Println("\narea: ", resPtr.area())
+	fmt.Println("perim:", resPtr.perim())
+
+	// ***NEW: Call method to modify original rect via pointer***
+	resPtr.resize(20, 30) // change width and height
+	fmt.Println("\nAfter resize -> area:", resPtr.area(), "perim:", resPtr.perim())
 }
