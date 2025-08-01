116330c44afc76a8cccb88c72d7350d5ea11fe1c
kubr <kubrtech@gmail.com>
2025-08-01 15:51:55 +0530

    iota, enums, string, and states implementation

--- golang/21_enums/main.go
+++ golang/21_enums/main.go
@@ -2,44 +2,72 @@ package main
 
 import "fmt"
 
-type ServerState int
+type State int
 
 const (
-	StateIdle ServerState = iota
-	StateConnected
-	StateError
-	StateRetrying
+	Start State = iota
+	Lunch
+	TeaBreak
+	Stumps
 )
 
-var stateName = map[ServerState]string{
-	StateIdle:      "idle",
-	StateConnected: "connected",
-	StateError:     "error",
-	StateRetrying:  "retrying",
+func (state State) String() string {
+	return []string{"Start", "Lunch", "TeaBreak", "Stumps"}[state]
 }
 
-func (ss ServerState) String() string {
-	return stateName[ss]
+// Alternative way to do it - using a map
+// var stateName = map[State]string{
+// 	Start:    "Start",
+// 	Lunch:    "Lunch",
+// 	TeaBreak: "TeaBreak",
+// 	Stumps:   "Stumps",
+// }
+
+// func (state State) String() string {
+// 	return stateName[state]
+// }
+
+// // Hard-coded String() without map
+// func (state State) String() string {
+// 	switch state {
+// 	case Start:
+// 		return "Start"
+// 	case Lunch:
+// 		return "Lunch"
+// 	case TeaBreak:
+// 		return "TeaBreak"
+// 	case Stumps:
+// 		return "Stumps"
+// 	default:
+// 		return "Unknown"
+// 	}
+// }
+
+func transition(state State) State {
+	switch state {
+	case Start:
+		return Lunch
+	case Lunch:
+		return TeaBreak
+	case TeaBreak:
+		return Stumps
+	case Stumps:
+		return Start
+	default:
+		panic(fmt.Errorf("unknown state: %d", state))
+	}
 }
 
 func main() {
-	ns := transition(StateIdle)
-	fmt.Println(ns)
+	start := transition(Start)
+	fmt.Println(start)
 
-	ns2 := transition(ns)
-	fmt.Println(ns2)
-}
+	lunch := transition(Lunch)
+	fmt.Println(lunch)
 
-func transition(s ServerState) ServerState {
-	switch s {
-	case StateIdle:
-		return StateConnected
-	case StateConnected, StateRetrying:
+	teaBreak := transition(TeaBreak)
+	fmt.Println(teaBreak)
 
-		return StateIdle
-	case StateError:
-		return StateError
-	default:
-		panic(fmt.Errorf("unknown state: %s", s))
-	}
+	stumps := transition(Stumps)
+	fmt.Println(stumps)
 }
